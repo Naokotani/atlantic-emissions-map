9b48c6149bc552a00bf1d22970eb57257805ed6b
Chris Hughes <naokotani@protonmail.com>
2025-01-12 16:14:48 -0400

    Inital setup of api
     - Added spring project
     - Added endpoints for health checks
     - Added Dockerfile and docker compose to run api and database
     - Updated README.md to reflect changes with run insructions.

---
+++ api/src/main/java/com/emissions/industrialemissionsmap/IndustrialEmissionsMapApplication.java
@@ -0,0 +1,13 @@
+package com.emissions.industrialemissionsmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustrialEmissionsMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndustrialEmissionsMapApplication.class, args);
+    }
+
+}
---
+++ api/src/test/java/com/emissions/industrialemissionsmap/IndustrialEmissionsMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emissions.industrialemissionsmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustrialEmissionsMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

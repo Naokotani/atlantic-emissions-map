654e55cd7c0dd299239ad9ec9eaeb9a8c9d1a603
Chris Hughes <naokotani@protonmail.com>
2025-01-20 13:34:01 -0400

    Added api functionality to frontend
    Use .env file to set the production and development API urls

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [emitter, setEmitter] = useState([]);
+  const apiUrl = import.meta.env.DEV?import.meta.env.VITE_DEV_API_URL:import.meta.env.VITE_PROD_API_URL
+
+
+  function getData() {
+    setCount(count + 1)
+    fetch(apiUrl + 'emitter/id?id=' + count)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setEmitter(data);
+      });
+
+  }
+  useEffect(() => {
+    getData(1)
+  }, []);
+
+  const [count, setCount] = useState(1)
 
   return (
     <>
@@ -19,16 +38,21 @@ function App() {
       <h1>Vite + React</h1>
       <h2>Hi Nick and Rose! 👋</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} derpy doo.
+        <button onClick={() => getData()}>
+          emitter number {count}.
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        My name is Fran. That's short for Francine?
-      </p>
+      <p>Version: {import.meta.env.DEV?"Development":"Production"}</p>
+    <div className="row">
+        {
+        (emitter)
+          ? <p> {emitter.facilityDescription}: {emitter.totalEmissions} tonnes of co2 equivalent</p>
+          : <p> Loading data ... </p> 
+        }
+    </div>
     </>
   )
 }

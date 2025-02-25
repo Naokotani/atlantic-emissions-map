5740ef0b41ce430192ede718136d3c0dd13d1aeb
Richardson902 <nickrichardson902@gmail.com>
2025-02-25 17:51:24 -0400

    Base map implementation

--- frontend/src/components/MapForm.jsx
+++ frontend/src/components/MapForm.jsx
@@ -1,3 +1,20 @@
-function MapForm() {}
+function MapForm({ filters, setFilters }) {
+  const handleYearChange = (e) => {
+    setFilters({ ...filters, year: e.target.value });
+  };
+
+  return (
+    <form>
+      <label htmlFor="year">Select Year:</label>
+      <p>Current filter: {filters.category}</p>
+      <select id="year" value={filters.year} onChange={handleYearChange}>
+        <option value="2023">2023</option>
+        <option value="2022">2022</option>
+        <option value="2021">2021</option>
+        <option value="2020">2020</option>
+      </select>
+    </form>
+  );
+}
 
 export default MapForm;
--- frontend/src/components/MapWrapper.jsx
+++ frontend/src/components/MapWrapper.jsx
@@ -1,6 +1,44 @@
+import { useState, useEffect } from "react";
+
 import Map from "./Map";
+
 import MapForm from "./MapForm";
 
-function MapWrapper() {}
+function MapWrapper() {
+  const [filters, setFilters] = useState({ year: "2022" }); //Default most current data (2023 empty)
+
+  const [mapData, setMapData] = useState([]); // Store fetched data
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `/api/v1/region/maritime?year=${filters.year}`
+        );
+
+        const data = await response.json();
+
+        console.log(data);
+
+        const allFacilities = Object.values(data).flat(); //Arrays inside arrays thing
+
+        setMapData(allFacilities); // Set data to be used in map
+      } catch (error) {
+        console.error("Error fetching map data:", error);
+      }
+    };
+
+    fetchData();
+  }, [filters.year]); // Re-fetch when the year filter changes
+
+  return (
+    <>
+      <MapForm filters={filters} setFilters={setFilters} />
+      <div>
+        <Map data={mapData} />
+      </div>
+    </>
+  );
+}
 
 export default MapWrapper;
